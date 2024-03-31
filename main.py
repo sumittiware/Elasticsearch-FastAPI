f76781ab91a8455b94a3f10ed32811cf196de175
Sumit Tiware <sumittiware@Sumits-MacBook-Pro.local>
2024-03-31 22:07:09 +0530

    feat : move to global, cachesd index apprroch

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ async def ping():
     return {"message": "pong"}
 
 @app.get("/search")
-async def search(query:str = None, state:str = None, response: fastapi.Response = None):
+async def search(query:str = None, state:str = None, result_type:str = None,response: fastapi.Response = None):
     state = "submitted" if state is None else state
     elastic_client = get_elasticsearch_client()
+    result_type = "course" if result_type is None else result_type
 
     if query is None :
         response.status_code = fastapi.status.HTTP_400_BAD_REQUEST
         return {"message": "No query provided"}
+    
     
     if state == "submitting" :
         
@@ -33,76 +35,64 @@ async def search(query:str = None, state:str = None, response: fastapi.Response 
         search_responses = []
 
         for hit in response_hits :
-            # first I will get the type if the response
-            types = get_response_type(hit["highlight"])
-            # then I will create a response based on type 
-            for type in types:
-                 search_response = get_search_response_data(type=type, data=hit)
-                 search_responses.append(search_response)
+           id = hit["_id"]
+           title = hit["_source"]["title"]
+           description = hit["_source"]["description"]
+           type = hit["_source"]["type"]
+           url = get_cta_url(type, title, id)
+
+           search_response = {
+               "id": id,
+               "title": title,
+               "description": description,
+               "type": type
+           }
+
+           search_responses.append(search_response)
+           
             
-        final_response = process_search_response(search_responses)
         return { 
                 "status":"ok", 
-                "data" : final_response
+                "data" : search_responses
             } 
     else :
-        elastic_search_response = elastic_client.search(index=search_index, body=get_submitted_query(query))
+        elastic_search_response = elastic_client.search(index=search_index, body=get_submitted_query(query,type=result_type))
         
         response_hits = elastic_search_response["hits"]["hits"]
 
-        course_ids = []
+        entity_ids = []
 
         for hit in response_hits :
-             course_ids.append(hit["_id"])
+             entity_ids.append(hit["_id"])
 
-        course_response = elastic_client.search(index=course_index, body=get_course_query(course_ids))
+        entity_response = elastic_client.search(index="search-f{result_type}-dev", body=get_id_query(entity_ids))
 
-        course_response_hits = course_response["hits"]["hits"]
+        entity_response_hits = entity_response["hits"]["hits"]
 
-        courses = []
+        results = []
 
-        for hit in course_response_hits :
-            course_hit = hit["_source"]
+        for hit in entity_response_hits :
+            entity_hit = hit["_source"]
 
-            course_data = {
+            entity_data = {
                 "id": hit["_id"],
-                "title": course_hit["name"],
-                "description": course_hit["description"],
-                "type": course_hit["type"],
-                "banners" : course_hit["banners"],
+                "title": entity_hit["name"],
+                "description": entity_hit["description"],
+                "type": entity_hit["type"],
+                "image" : entity_hit["image"],
+                # TODO : This can chnage based on the entity type
+                "cta_url": f"/course/detail/{hit['_id']}"
             }
 
-            courses.append(course_data)
+            results.append(entity_data)
 
-        return {"status":"ok","data" : courses}
+        return {
+            "status":"ok",
+            "type" : result_type,
+            "data" : results
+        }
 
 
-def process_search_response(data):
-    categories_seen = set()
-    processed_data = []
-
-    # First, process categories to ensure uniqueness
-    for item in data:
-        if isinstance(item, list):  # Assuming lists contain categories
-            for category in item:
-                title = category["title"]
-                if title not in categories_seen:
-                    categories_seen.add(title)
-                    processed_data.append({"type": "category", **category})
-
-    # Then, process courses and instructors
-    for item in data:
-        if isinstance(item, dict):  # Assuming dicts are courses or instructors
-            if item["description"] == "category":
-                continue  # Skip categories in this pass
-            item_type = "course" if "course" in item["url"] else "instructor"
-            processed_data.append({"type": item_type, **item})
-
-    # Sort the data: categories -> courses -> instructors
-    type_order = {"category": 1, "course": 2, "instructor": 3}
-    processed_data.sort(key=lambda x: type_order[x["type"]])
-
-    return processed_data
 
 
 if __name__ == "__main__":
